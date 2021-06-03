da1da924d6983ecd5dd550da0c7dbd21ced23234
Joma Simeon <simjoms@gmail.com>
2021-06-03 16:33:24 +0300

    update firebase perm

--- src/index.js
+++ src/index.js
@@ -6,19 +6,9 @@ import { connectDb } from '../src/models/index'
 import questionRouter from './routes/question'
 
 import admin from 'firebase-admin'
+import { getSecret } from './utils/gcp_secrets'
 var firebaseMiddleware = require('express-firebase-middleware')
 let serviceAccount
-
-//firebase admin permissions
-if (process.env.GOOGLE_CLOUD_PROJECT) {
-  serviceAccount = process.env.FIREBASE_ADMIN_PERMISSIONS
-} else {
-  serviceAccount = require('../perm.json')
-}
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-})
 
 const app = express()
 app.use(cors())
@@ -27,5 +17,17 @@ app.use(express.json())
 app.use('/api/v1/question', firebaseMiddleware.auth, questionRouter)
 
 connectDb().then(async () => {
+  //firebase admin permissions
+
+  if (process.env.GOOGLE_CLOUD_PROJECT) {
+    serviceAccount = await getSecret(process.env.FIREBASE_ADMIN_PERMISSIONS)
+  } else {
+    serviceAccount = require('../perm.json')
+  }
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  })
+
   app.listen(8080, () => console.log(`app listening on port 8080`))
 })
