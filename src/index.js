226520d618b09a7c32d79d64d35b3fc79c5c9a82
Joma Simeon <simjoms@gmail.com>
2021-06-03 21:16:15 +0300

    fix google application credentials

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import { connectDb } from '../src/models/index'
 import questionRouter from './routes/question'
 
 import admin from 'firebase-admin'
-import { getSecret } from './utils/gcp_secrets'
 var firebaseMiddleware = require('express-firebase-middleware')
-let serviceAccount
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+})
 
 const app = express()
 app.use(cors())
@@ -17,17 +19,5 @@ app.use(express.json())
 app.use('/api/v1/question', firebaseMiddleware.auth, questionRouter)
 
 connectDb().then(async () => {
-  //firebase admin permissions
-
-  if (process.env.GOOGLE_CLOUD_PROJECT) {
-    serviceAccount = await getSecret(process.env.FIREBASE_ADMIN_PERMISSIONS)
-  } else {
-    serviceAccount = require('../perm.json')
-  }
-
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  })
-
   app.listen(8080, () => console.log(`app listening on port 8080`))
 })
