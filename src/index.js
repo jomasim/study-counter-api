739be9add6aa5ee2a0ffe3516916231675d3ad7f
Joma Simeon <simjoms@gmail.com>
2021-06-04 17:39:13 +0300

    refactor code

--- src/index.js
+++ src/index.js
@@ -19,7 +19,19 @@ connectDb().then(async () => {
   let params
   if (process.env.GOOGLE_CLOUD_PROJECT) {
     const data = await getSecret(process.env.FIREBASE_PERMISSIONS)
-    params = JSON.parse(data)
+    const result = JSON.parse(data)
+    params = {
+      type: result.type,
+      projectId: result.project_id,
+      privateKeyId: result.private_key_id,
+      privateKey: result.private_key,
+      clientEmail: result.client_email,
+      clientId: result.client_id,
+      authUri: result.auth_uri,
+      tokenUri: result.token_uri,
+      authProviderX509CertUrl: result.auth_provider_x509_cert_url,
+      clientC509CertUrl: result.client_x509_cert_url
+    }
   } else {
     params = process.env.FIREBASE_PERMISSIONS
   }
