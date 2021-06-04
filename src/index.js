0eecf9b125cf4a466863b70e159eca256f9c3734
Joma Simeon <simjoms@gmail.com>
2021-06-04 17:52:06 +0300

    params fix

--- src/index.js
+++ src/index.js
@@ -19,19 +19,7 @@ connectDb().then(async () => {
   let params
   if (process.env.GOOGLE_CLOUD_PROJECT) {
     const data = await getSecret(process.env.FIREBASE_PERMISSIONS)
-    const result = JSON.parse(data)
-    params = {
-      type: result.type,
-      projectId: result.project_id,
-      privateKeyId: result.private_key_id,
-      privateKey: result.private_key,
-      clientEmail: result.client_email,
-      clientId: result.client_id,
-      authUri: result.auth_uri,
-      tokenUri: result.token_uri,
-      authProviderX509CertUrl: result.auth_provider_x509_cert_url,
-      clientC509CertUrl: result.client_x509_cert_url
-    }
+    params = JSON.parse(data)
   } else {
     params = process.env.FIREBASE_PERMISSIONS
   }
