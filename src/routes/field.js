bf7d3762145bc5b779247cebb3d69910ffe359d0
Joma Simeon <simjoms@gmail.com>
2021-09-07 08:56:20 +0300

    update feild route

--- src/routes/field.js
+++ src/routes/field.js
@@ -7,7 +7,7 @@ const fieldRouter = new express.Router()
 
 fieldRouter.get('/', (req, res) => fieldController.list(req, res))
 
-fieldRouter.post('/', firebaseMiddleware.auth, validate.field, (req, res) =>
+fieldRouter.post('/', validate.field, (req, res) =>
   fieldController.add(req, res)
 )
 
